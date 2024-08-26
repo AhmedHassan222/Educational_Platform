f97f08ac3eca0273238a0d02b0519523ea3e3fda
Ahmed Hassan <ah2300487@gmail.com>
2024-08-26 13:56:39 +0200

    update

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,18 +1,17 @@
 import style from "../Styles/Nav.module.css"
+import logo from "../../src/Assets/Images/logo.png"
 import { Link } from "react-router-dom"
 export default function Navbar() {
     return <>
-        <nav className="navbar navbar-expand-lg  navbar-dark py-2 fixed-top">
+        <nav className="navbar navbar-expand-lg  ">
             <div className="container">
-                <Link className="navbar-brand" to={'/'}>Navbar</Link>
+                <Link className="navbar-brand" to={'/'}>
+                <img src={logo} alt="sky academy logo" className={`${style.widthLogo}`} /></Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
-                            <Link className="nav-link active" aria-current="page" to={'/'}>Home</Link>
-                        </li>
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
                             <Link className="nav-link" to={'/login'}>Login</Link>
                         </li>
