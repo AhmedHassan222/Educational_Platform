bca8beb6aea258f677231d49fee5cc81138a3ba7
Ahmed Hassan <ah2300487@gmail.com>
2024-09-12 16:57:03 +0200

    update

--- src/Pages/AddCourse.jsx
+++ src/Pages/AddCourse.jsx
@@ -1,99 +1,116 @@
-import { useState ,useContext} from "react";
+import { useState, useContext, useEffect } from "react";
 import style from "../../src/Styles/Auth.module.css"
 import Cookies from 'js-cookie';
-import { CRUDContext } from "../Contexts/CRUDContext";
 import axios from "axios";
 import Joi from "joi";
 import { useNavigate } from "react-router-dom";
 export default function AddCourse() {
-  
-
-    let navigate =useNavigate()
+    // VARIABLE >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+    const navigate = useNavigate()
     const [error, setError] = useState([]);
     const [errorForm, seterrorForm] = useState("");
-    const {baseURL}=useContext(CRUDContext)
-    const [addCourse, setaddCourse] = useState({
-        name:"",
-    });
+    const [subcategoryId, setSubCategoryId] = useState(null);
+    const [subCategories, setSupCategories] = useState([])
+    const baseURL = `https://ahmed-shaltout-platform.up.railway.app`;
+    const grade = { primary: "الابتدائي", preparatory: "الاعدادي ", secondary: "الثانوي" };
+    const [name, setName] = useState({ name: "" });
+    const [image, setImage] = useState(null);
     const [Isloading, setIsloading] = useState(false);
+    const formData = new FormData();
+    // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+    // FUNCTION >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+    // FUNCTION HANDLE IMAGE >>
+    const handleImageChange = (e) => {
+        const file = Array.from(e.target.files)[0];
+        setImage(file);
+  };
+    // FUNCTION  GET ALL SUB  CATEGORIES >>
+    async function getAll() {
+        const { data } = await axios.get(`${baseURL}/subcategory/`);
+        setSupCategories(data.Subcategories)
+    }
 
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setaddCourse({
-            ...addCourse,
-            [name]: value,
-        });
-    };
+    useEffect(() => {
+        getAll();
+    }, [])
+    //  >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+   
     const validationForm = () => {
         let schema = Joi.object({
+            image: Joi.array(),
             name: Joi.string().required(),
         });
-        return schema.validate(addCourse, { abortEarly: false });
+        return schema.validate(formData, { abortEarly: false });
     };
     const handleSubmit = (e) => {
         setIsloading(true)
         e.preventDefault();
-        const validate = validationForm();
-        if(validate.error){
-            setError(validate.error.details) 
-        }else{
-            addItem()
-        }
-        setIsloading(false)
-        console.log(error)
+        // const validate = validationForm();
+        // if (validate.error) {
+        //     setError(validate.error.details)
+        // } else {
+        //     addItem()
+        // }
+        // setIsloading(false)
+        // console.log(error)
+        addItem();
     };
     async function addItem() {
-   try {
-    await axios.post(`${baseURL}/create?subCategoryId=`, addCourse, {
-        headers: {
-            "token": `online__${Cookies.get('token')}`
+        formData.append("image",image);
+        formData.append("name",name);
+        try {
+            await axios.post(`${baseURL}/course/create?subCategoryId=${subcategoryId}`, formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                    "token": `online__${Cookies.get('token')}`
+                }
+            }).then((res) => {
+                console.log(res)
+                // navigate('/admin/allCources')
+            })
+        } catch (error) {
+            console.log(error)
+            // seterrorForm(error)
         }
-    }).then((res)=>{
-        console.log(res)
-        navigate('/admin/allCources')
-    })
-   } catch (error) {
-    // console.log(error)
-    seterrorForm(error)
-   }
-}
+    }
     return <>
         <div className="container py-5">
             <div className="text-center rounded-4  border-1 widthCustom mx-auto">
-            <form encType="multipart/form-data" onSubmit={handleSubmit}>
-               
-                <div className=" mb-4">
-                    <input
-                        placeholder=" اضف صورة "
-                        type="file"
-                        className="w-100 p-2"
-                        id="fullName"
-                        name="fullName"
-                        onChange={handleChange}
-                        required
-                    />
-                </div>
-                <div className=" mb-4">
-                    <input
-                        placeholder=" العنوان "
-                        type="text"
-                        className="w-100 p-2"
-                        id="fullName"
-                        name="fullName"
-                        onChange={handleChange}
-                        required
-                    />
-                </div>
-                <div className=" mb-4">
-                    <select className="w-100 p-2" name="category" id="">
-                        <option value="thered">الصف الثالث الثانوي</option>
-                        <option value="second">الصف الثاني الثانوي</option>
-                    </select>
-                </div>
-              
-
-                <button type="submit" className={`w-100 p-2 border-0 rounded-2 ${style.btnOrange} my-3  w-100 `}> اضف</button>
-            </form>
+                <form encType='multibart/form-data' onSubmit={handleSubmit}>
+                    <div className=" mb-4">
+                        <input
+                            placeholder=" اضف صورة "
+                            type="file"
+                            className="w-100 p-2"
+                            id="image"
+                            name="image"
+                            onChange={handleImageChange}
+                        />
+                    </div>
+                    <div className=" mb-4">
+                        <input
+                            placeholder=" اضف  عنوانا للكورس "
+                            type="text"
+                            className="w-100 p-2"
+                            id="name"
+                            name="name"
+                            value={name.name}
+                            onChange={(e)=>setName(e.target.value)}
+                        />
+                    </div>
+                    <div className="my-4">
+                        <select className="w-100 p-2 text-muted" id="name" name="name" onChange={(e) => setSubCategoryId(e.target.value)}  >
+                            <option value="">  المرحلة الدراسية </option>
+                            {subCategories.map((subCategory, index) => { <option key={index} value={subCategory.id}>{grade[subCategory.name]}</option> })}
+                        </select>
+                        {error?.map((err, index) =>
+                            err.context.label === "name" ? <div key={index}>
+                                {!formData.name ? <p className="small fw-medium  py-2 text-end text-danger">لا يمكن ارسال هذا الحقل  فارغا</p> : ""}
+                            </div> : ""
+                        )}
+                    </div>
+                    <button type="submit" className={`w-100 p-2 border-0 rounded-2 ${style.btnOrange} my-3  w-100 `}> اضف</button>
+                </form>
             </div>
         </div>
     </>
--- src/Pages/GetAllCources.jsx
+++ src/Pages/GetAllCources.jsx
@@ -1,27 +1,115 @@
-export default function GetAllCources() {
-    const arr = [1,2,3,4,5,6,7,8,9]
-    return <>
-        <div className="container py-5">
-            <table className="table table-striped  table-hover table-bordered">
-                <thead>
-                    <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">First</th>
-                        <th scope="col">Last</th>
-                        <th scope="col">Handle</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {arr.map((item ,index) => <tr key={index}>
-                        <th scope="row">1</th>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                    </tr>
-                   )}
-                </tbody>
 
-            </table>
-        </div>
-    </>
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Cookies from "js-cookie";
+import { Link } from "react-router-dom";
+
+export default function GetAllCources() {
+    let arr = [1, 2, 3, 4];
+    const baseURL = `https://ahmed-shaltout-platform.up.railway.app`;
+    const [Courses, setCourses] = useState([]);
+    const [errorForm, seterrorForm] = useState("");
+    let stage = {
+        first: "الصف الاول",
+        second: " الصف الثاني",
+        third: "الصف الثالث",
+        fourth: "الصف الرابع",
+        fifth: "الصف الخامس",
+        sixth: "الصف السادس"
+    };
+    let grade = {
+        primary: "الابتدائي",
+        preparatory: "الاعدادي ",
+        secondary: "الثانوي",
+    };
+    let date = new Date();
+    async function getAll() {
+        const { data } = await axios.get(`${baseURL}/course/`);
+        // setCourses(data)
+        console.log(data)
+    }
+
+    async function deleteItem(id) {
+        try {
+            await axios
+                .delete(`${baseURL}/subcategory/delete?subCategoryId=${id}`, {
+                    headers: {
+                        token: `online__${Cookies.get("token")}`,
+                    },
+                })
+        } catch (error) {
+            console.log(error);
+            seterrorForm(error.message)
+        }
+
+    }
+
+    useEffect(() => {
+        getAll();
+    }, [Courses]);
+
+    return (
+        <>
+            <div className="container py-5">
+                <table className="table table-striped text-center  table-hover table-bordered">
+                    <thead>
+                        <tr>
+                            <th className="py-3" scope="col">
+                                #
+                            </th>
+                            <th className="py-3" scope="col">
+                                صورة الكورس
+                            </th>
+                            <th className="py-3" scope="col">
+                                العنوان
+                            </th>
+                            <th className="py-3" scope="col">
+                                تاريخ الانشاء
+                            </th>
+                            <th className="py-3" scope="col">
+                                المعاملات{" "}
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {Courses?.length > 0
+                            ? Courses.map((item, index) => (
+                                <tr key={index}>
+                                    <th scope="row">{index + 1}</th>
+                                    <td>{stage[item.name]} {grade[item.categoryId.name]}</td>
+                                    <td>{date.toISOString(item.createdAt).split("T")[0]}</td>
+                                    <td className="d-flex justify-content-center justify-content-center">
+                                        <button
+                                            className="btn btn-sm btn-danger ms-2"
+                                            onClick={() => { deleteItem(item.id); }}
+                                        >
+                                            حذف
+                                        </button>
+                                        <div>
+                                            <Link
+                                                className="btn btn-primary"
+                                                to={`/admin/updatesubcategory/${item.name}/${item.id}`}
+                                            >
+                                                تعديل
+                                            </Link>
+
+
+                                        </div>
+                                    </td>
+                                </tr>
+                            ))
+                            : arr.map((item, index) => (
+                                <tr key={index}>
+                                    <th className="placeholder-glow   p-3"></th>
+                                    <td className="placeholder-glow   p-3"></td>
+                                    <td className="placeholder-glow   p-3"></td>
+                                    <td className="placeholder-glow   p-3"></td>
+                                </tr>
+                            ))}
+                    </tbody>
+                </table>
+                {errorForm ? <p className="text-danger py-1 text-center small"> لديك مشكلة في اخر عملية </p> : ''}
+            </div>
+        </>
+    );
 }
